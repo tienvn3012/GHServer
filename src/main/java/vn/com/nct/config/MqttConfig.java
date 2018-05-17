806074ee08e7e1cc37f48dc471c307c50a33cfc3
tienvn <tienvn3012@gmail.com>
2018-05-17 10:49:18 +0700

    fix EC equation

--- src/main/java/vn/com/nct/config/MqttConfig.java
+++ src/main/java/vn/com/nct/config/MqttConfig.java
@@ -14,7 +14,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import vn.com.nct.model.Devices;
+import vn.com.nct.model.response.DevicesResponse;
 import vn.com.nct.service.controlservice.AutomaticControlService;
+import vn.com.nct.service.objectservice.ObjectService;
 
 
 @Configuration
@@ -35,6 +38,10 @@ public class MqttConfig implements MqttCallback{
 	@Autowired
 	@Qualifier(value = "automaticControl")
 	private AutomaticControlService automatic;
+	
+	@Autowired
+	@Qualifier("devicesService")
+	private ObjectService<Devices, DevicesResponse> deviceService;
 
 	private MqttClient subscriber 			 = null;
 	private MqttClient publisher 			 = null; // for led
@@ -184,6 +191,18 @@ public class MqttConfig implements MqttCallback{
 		System.out.println("Receive message '"+message+"' from topic '"+topic+"'");
 		
 		String[] split = message.toString().split(";");
+		int id = Integer.parseInt(split[0]);
+		
+		Devices d = deviceService.getOneById(id);
+		if(d == null) {
+			System.out.println("Reject message cause device not found !!!!");
+			return;
+		}
+		if(!d.isDevice_status()) {
+			System.out.println("Reject message cause device've not loged in !!!!");
+			return;
+		}
+		
 		try {
 			publisher.publish("nct_collect_"+split[0], message.getPayload(), 0, false);
 			System.out.println("publish message \""+message.toString()+"\" to topic nct_collect_"+split[0]);
